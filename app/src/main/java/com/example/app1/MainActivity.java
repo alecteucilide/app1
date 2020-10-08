dbd6ef5a3de925146ce97c7ab0d35431e651f942
alecteucilide <alecadrianteucilide@gmail.com>
2020-10-08 17:50:50 +0200

    First commit

---
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.app1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Toast.makeText(MainActivity.this, "Chin chin", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
